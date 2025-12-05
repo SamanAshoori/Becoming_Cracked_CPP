e89865797867c696706b1463c0f1693b80859b1b
Saman Ashoori <saman@samanashoori.com>
2025-12-05 18:29:12 +0000

    Enhance Rock Paper Scissors game logic with score tracking and improved output

--- Rock_Paper_Scissors/rock_paper_scissors.cpp
+++ Rock_Paper_Scissors/rock_paper_scissors.cpp
@@ -12,6 +12,10 @@ int main()
     //set max and min for random number
     int max = 3;
     int min = 1;
+    //set player and computer score
+    int playerScore = 0;
+    int computerScore = 0;
+    int gamesPlayed = 0;
 
     // Create an unordered_map
     std::unordered_map<int,std::string> umap;
@@ -21,36 +25,65 @@ int main()
     umap[2] = "Paper"; 
     umap[3] = "Scissors"; 
 
-    std::cout << "Welcome to Rock Paper Scissors - Please 1 for Rock , 2 for Paper and 3 for Scissors: ";
-    int x{}; //defining variable to hold user input
 
-    std::cin >> x; //get number from user
-    int range = max - min + 1;
-    int computerChoice = rand() % range + min;
+    while (playerScore != 3 || computerScore != 3 )
+    {
+        std::cout << "Welcome to Rock Paper Scissors - Please Enter 1 for Rock , 2 for Paper and 3 for Scissors: ";
+        int x{}; //defining variable to hold user input
 
-    std::cout << "You entered: " << umap[x] << '\n';
-    std::cout << "Computer entered: " << umap[computerChoice] << '\n';
+        std::cin >> x; //get number from user
+        int range = max - min + 1;
+        int computerChoice = rand() % range + min;
 
-    //SADLY GOING TO IF STATEMENT MAX as my current knowledge of c++ is not advanced yet
-    if(x == computerChoice){
-        std::cout << "Draw" << '\n';
-    }
-    //Will else if all player wins and any else is computer win
-    //player rock and computer rock
-    else if(x == 1 && computerChoice == 3){
-        std::cout << "You Win" << '\n';
-    }
-    //player paper and computer rock
-    else if(x == 2 && computerChoice == 1){
-        std::cout << "You Win" << '\n';
-    }
-    //player scissors and computer paper
-    else if(x == 3 && computerChoice == 2){
-        std::cout << "You Win" << '\n';
-    }
-    else{
+        std::cout << "You entered: " << umap[x] << '\n';
+        std::cout << "Computer entered: " << umap[computerChoice] << '\n';
+
+        //SADLY GOING TO IF STATEMENT MAX as my current knowledge of c++ is not advanced yet
+        if(x == computerChoice)
+        {
+            std::cout << "Draw" << '\n';
+        }
+        //Will else if all player wins and any else is computer win
+        //player rock and computer rock
+        else if(x == 1 && computerChoice == 3)
+        {
+            std::cout << "You Win" << '\n';
+            playerScore = playerScore + 1;
+            std::cout << "Player Score " << playerScore << '\n';
+        }
+        //player paper and computer rock
+        else if(x == 2 && computerChoice == 1)
+        {
+            std::cout << "You Win" << '\n';
+            playerScore = playerScore + 1;
+           std::cout << "Player Score " << playerScore << '\n';
+        }
+        //player scissors and computer paper
+        else if(x == 3 && computerChoice == 2)
+        {
+            std::cout << "You Win" << '\n';
+            playerScore = playerScore + 1;
+            std::cout << "Player Score " << playerScore << '\n';
+        }
+        else
+        {
         std::cout << "Computer Won" << '\n';
+        computerScore = computerScore +1;
+        std::cout << "Computer Score " << computerScore << '\n';
+        }
+        gamesPlayed = gamesPlayed + 1;
     }
+    if(playerScore == 3)
+    {
+        std::cout << "You Won!" << '\n';
+        std::cout << "Games Player " << gamesPlayed; 
+    }
+    else
+    {
+        std::cout << "Computer Won!" << '\n';
+        std::cout << "Games Player " << gamesPlayed; 
+    }
+
     
 
 }

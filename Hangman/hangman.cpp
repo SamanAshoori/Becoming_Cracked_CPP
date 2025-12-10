b48a293543ca95ff3adb4a39ba18c1b34d63e5a4
Saman Ashoori <saman@samanashoori.com>
2025-12-10 00:02:13 +0000

    Refactor code structure for improved readability and maintainability

--- Hangman/hangman.cpp
+++ Hangman/hangman.cpp
@@ -6,6 +6,36 @@
 #include <ctime>
 #include <vector> //for scoring
 
+std::string selectRandomWord();
+
 int main(){
+    std::cout << selectRandomWord();
+}
+
+std::string selectRandomWord(){
     
+
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    int max = 10;
+    int min = 1;
+    int range = max - min + 1;
+
+    int randomNum = rand() % range + min;
+    
+    std::string words[10] = {
+        "programmer",
+        "language",
+        "python",
+        "string",
+        "programming",
+        "cascade",
+        "electromagnetic",
+        "whisper",
+        "vortex",
+        "luminance"
+    };
+    std::string word = words[randomNum];
+
+
+    return word;
 }
